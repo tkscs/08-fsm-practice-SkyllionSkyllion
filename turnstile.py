54c1d4113e7bcc3d4b0abd6558fc3e96dce5b189
SkyllionSkyllion <kstanat2028@kehillah.org>
2025-11-18 10:11:33 -0800

    yay

--- turnstile.py
+++ turnstile.py
@@ -1,1 +1,21 @@
 # write code to implement a turnstile
+
+state = "Locked"
+while state != "Walked":
+    while state == "Locked":
+        state = input ("Turnstile is currently Locked. Would you like to Push or Coin.  ")
+        if state == "Push":
+            print ("Turnstile is Locked. Please insert coin.  ")
+            state = "Locked"
+        elif state == "Coin":
+            print ("Coin has been deposited. Turnstile is now Unlocked.  ")
+            state = "Unlocked"
+    while state == "Unlocked":
+        state = input ("Turnstile is currently Unlocked. Would you like to Push or Coin.  ")
+        if state == "Push":
+            print ("You have walked through the Turnstile. Have a good day.  ")
+            state = "Walked"
+        elif state == ("Coin"):
+            print("You have already inserted a coin dummy. Please walk through.  ")
+            state = "Unlocked"
+            
